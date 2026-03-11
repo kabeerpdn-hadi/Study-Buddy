db8d2dcd8373f095feb9d12d032dde84ca6a8fce
programmer_hadi <kabeerpdn@gmail.com>
2026-03-11 05:32:39 +0400

    fixing infinite loop

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -154,7 +154,7 @@ export const refreshToken = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 15 * 60 * 1000,
     });
 
