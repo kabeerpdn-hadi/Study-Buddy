1b9f98b45a6a67556785b1c835899ef499e4de97
programmer_hadi <kabeerpdn@gmail.com>
2026-03-11 21:21:45 +0400

    fixing spelling issue

--- backend/models/feedback.model.js
+++ backend/models/feedback.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const feedbackSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     message: { type: String, required: true },
-    type: { type: String, enum: ["feedback", "complain"], required: true },
+    type: { type: String, enum: ["feedback", "complaint"], required: true },
 }, {timestamps: true}
 );
 
