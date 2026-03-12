4718dd76cc1263540679cd509574b1e39ed2d77c
programmer_hadi <kabeerpdn@gmail.com>
2026-03-12 05:16:26 +0400

    trying to fix mobile isue

--- frontend/src/libraries/axios.js
+++ frontend/src/libraries/axios.js
@@ -4,7 +4,7 @@ import useUserStore from "../store/useUserStore"
 const removeUser = () => useUserStore.getState().setUser(null)
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "",
   withCredentials: true,
 });
 
