31d4a715f1f9d31bd88311a6a690c61ed5234a14
programmer_hadi <kabeerpdn@gmail.com>
2026-03-12 15:03:14 +0400

    trying to fix overlap issue

--- frontend/src/components/BottomNav.jsx
+++ frontend/src/components/BottomNav.jsx
@@ -6,7 +6,7 @@ const BottomNav = () => {
     const [isMoreOpen, setIsMoreOpen] = useState(false)
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 flex md:hidden bg-gray-900 p-4 justify-around">
+        <div className="fixed bottom-0 left-0 right-0 flex md:hidden bg-gray-900 p-4 justify-around z-50">
             <Link to="/dashboard" className="flex flex-col items-center text-gray-400 hover:text-white">
                 <LayoutDashboard size={24} />
                 <p className="text-xs">Dashboard</p>
@@ -25,7 +25,7 @@ const BottomNav = () => {
             </Link>
             {isMoreOpen && (
                 <div className="fixed bottom-20 right-4 bg-gray-800 rounded-2xl p-4 flex flex-col gap-3">
-                    <Link to="/feedback" className="text-gray-300 hover:text-white font-bold" onClick={() => setIsMoreOpen(false)}>
+                    <Link to="/feedback" className="flex items-center gap-2 text-gray-300 hover:text-white font-bold" onClick={() => setIsMoreOpen(false)}>
                         <MessageSquareText size={24} />
                         <p className="text-xs">Feedback</p>
                     </Link>
